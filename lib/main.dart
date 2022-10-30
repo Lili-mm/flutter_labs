2a56b17240a58f940790fa81168cd7a4e6c3628c
Lili-mm <lilymelnychuk1@gmail.com>
2022-10-30 12:20:34 +0200

    schedule code

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,62 @@
 import 'package:flutter/material.dart';
-import './Page2.dart';
-
+import 'schedule.dart';
+import 'homepage.dart';
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+
+      home:  MyApp()
+    ),
+  );
 }
+
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Page1()
-    );
-  }
-}
-
-class Page1 extends StatelessWidget {
-  Page1({super.key});
-String login = '';
-  // This widget is the root of your application.
+  MyApp({super.key});
+  ScheduleData scheduleData = ScheduleData();
+  late List<String> groups = scheduleData.getGroupsNames();
+  late String? selectedGroup = groups[0];
+  late Schedule schedule = scheduleData.getScheduleForGroup(selectedGroup);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login Page')
+        backgroundColor: Colors.red,
+        title: Text('Groups'),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
-          children: <Widget>[
-            Container(child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: ('Input Login'),
+          children: [
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: DropdownButton<String>(
+                  value: selectedGroup,
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  items: groups
+                      .map((group) =>
+                      DropdownMenuItem(
+                        value: group,
+                        child: Text(group),
+                      ))
+                      .toList(),
+                  onChanged: (String? newGroup) {
+                    selectedGroup = newGroup!;
+                  }
               ),
-              onChanged: (text) {login = text;}
-            ),),
-            Container(child: ElevatedButton(
-              child: Text('Login'),
-              onPressed: (){Navigator.of(context).push(MaterialPageRoute(builder: (context) => Page2(login:login)));}
-            )),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) =>
+                          Home(selectedGroup: selectedGroup)));
+                },
+                child: Text('Продовжити')),
+
           ],
         ),
-    ));
+
+      ),
+    );
   }
 }
-
